cc0520115e9833a1bc07c58b711682f2bac44b93
Yair Rodriguez <yairandrezunig1@gmail.com>
2025-08-05 11:57:52 -0600

    Add files via upload

---
+++ Base - App - RegistroMedico/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,59 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @Transactional
+    @PostMapping
+    public void registrar(@RequestBody @Valid DatosRegistroMedico datos) {
+        repository.save(new Medico(datos));
+    }
+
+    @GetMapping
+    public PagedModel<EntityModel<DatosListaMedico>> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
+        Page<DatosListaMedico> pagina = repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+        return pagedResourcesAssembler.toModel(pagina, datosListaMedicoModelAssembler);
+    }
+
+    @Autowired
+    private PagedResourcesAssembler<DatosListaMedico> pagedResourcesAssembler;
+    @Autowired
+    private DatosListaMedicoModelAssembler datosListaMedicoModelAssembler;
+
+    @Transactional
+    @PutMapping
+    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos) {
+        var medico = repository.getReferenceById(datos.id());
+        medico.actualizarInformaciones(datos);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.eliminar();
+    }
+
+
+
+
+
+
+
+}
